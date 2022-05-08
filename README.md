a12ebc25f59b4144e7343ab0536b46d1ae1c3069
TheLaluka <louka.jacques-chevallier@manomano.com>
2022-05-08 14:59:53 +0200

    started python rework

--- README.md
+++ README.md
@@ -1,33 +1,44 @@
-# bypass-url-parser
+# Bypass Url Parser
 
-Tool that tests `MANY` url bypass to reach a `40X protected page`.
+Tool that tests `MANY` url bypasses to reach a `40X protected page`.
 
 
-## How to, Setup, Use
+## Setup for bypass.py
 
 ```bash
-# Setup
-git clone https://github.com/laluka/bypass-url-parser
-sudo mv bypass-url-parser /opt/
-# Add this to your aliases
-alias bypass='f(){ /opt/bypass-url-parser/bypass.sh -u $@ 2>&1 | tee bypass-$(date +%s%N).log;  unset -f f; }; f'
-# Use it
-./bypass.sh http://127.0.0.1/
-./bypass.sh http://127.0.0.1/blocked/path/
+virtualenv -p python3 .py3
+source .py3/bin/activate
+pip install -r requirements.txt
+./bypass-url-parser.py http://127.0.0.1/juicy_403_endpoint/
 ```
 
 
-## Dirty code disclaimer
+## Usage
 
-The code is bad. Like, really bad.
+```
+./bypass-url-parser.py -h
+Bypass Url Parser, made with love by @TheLaluka
+A tool that tests MANY url bypasses to reach a 40X protected page.
 
-A first python-base approach was made, but the issue if that while we're not using raw sockets, too many wrappers are decoding/encoding the url before sending it to the server, so it was a pain to send our actual payloads to the server, thus here we are, in 2021, using `curl --path-as-is -skg`, and it works smoothly!
+Usage:
+    ./bypass-url-parser.py --url=<URL> [--outdir=<OUTDIR>] [--debug]
+    ./bypass-url-parser.py (-h | --help)
+    ./bypass-url-parser.py (-v | --version)
+
+Options:
+    -h --help            Show help, you are here :)
+    -v --version         Show version info.
+    --url=<URL>          URL (path is optional) to run bypasses against.
+    --outdir=<outdir>    Output directory for results.
+    --debug              Enable debugging output, to... Tou know... Debug.
+
+Example:
+    ./bypass-url-parser.py http://127.0.0.1/juicy_403_endpoint/
+```
 
 
-## Contribute
+## Expected result
 
-1. Fork this repo
-1. `git checkout -b "$USER/$FEATURE"`
-1. Implement your feature / refactor
-1. `git push --set-upstream origin "$USER/$FEATURE"`
-1. Create pull request that we'll be happy to review :)
+```
+TODO: add sample run once it works :)
+```
---
+++ bypass-url-parser.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+"""Bypass Url Parser, made with love by @TheLaluka
+A tool that tests MANY url bypasses to reach a 40X protected page.
+
+Usage:
+    ./bypass-url-parser.py --url=<URL> [--outdir=<OUTDIR>] [--debug]
+    ./bypass-url-parser.py (-h | --help)
+    ./bypass-url-parser.py (-v | --version)
+
+Options:
+    -h --help            Show help, you are here :)
+    -v --version         Show version info.
+    --url=<URL>          URL (path is optional) to run bypasses against.
+    --outdir=<outdir>    Output directory for results.
+    --debug              Enable debugging output, to... Tou know... Debug.
+
+Example:
+    ./bypass-url-parser.py http://127.0.0.1/juicy_403_endpoint/
+"""
+
+from docopt import docopt
+from pathlib import Path
+from rich import print
+import coloredlogs
+import json
+import logging
+import os
+import re
+import sys
+import tempfile
+import urllib
+
+VERSION = "0.1.0"
+logger = logging.getLogger("bup")
+config = dict()
+
+
+class Bypasser:
+    def __init__(self, config):
+        self.url = config.get("url")
+        self.outdir = config.get("outdir")
+        self.debug = config.get("debug")
+
+    def __str__(self):
+        out = str()
+        out += f"url: {self.url}"
+        out += f"outdir: {self.outdir}"
+        out += f"debug: {self.debug}"
+        return out
+
+    def stage_1(self):
+        logger.warning("Stage: stage_1")
+        return
+
+    def stage_2(self):
+        logger.warning("Stage: stage_2")
+        return
+
+    def stage_3(self):
+        logger.warning("Stage: stage_3")
+        return
+
+    def save_and_quit(self):
+        logger.warning("Stage: save_and_quit")
+        # path_routes = config["outdir"] + "/all-routes.json"
+        # with open(path_routes, "w") as f:
+        #     f.write(json.dumps(config["routes"], indent=4, sort_keys=True))
+        # 
+        # path_urls = config["outdir"] + "/all-urls.lst"
+        # with open(path_urls, "w") as f:
+        #     f.write("\n".join(sorted(set(config["final_urls"]))))
+        # 
+        # logger.info(f"Saving routes & urls in: \n{path_routes}\n{path_urls}")
+        # logger.debug(config["final_urls"])
+        return
+
+
+def main():
+    global config
+    # Show all options by Default
+    if len(sys.argv) == 1:
+        sys.argv.append("-h")
+    arguments = docopt(__doc__, version=f"bypass-url-parser {VERSION}")
+
+    # debug
+    config["debug"] = arguments.get("--debug")
+
+    # Log level
+    coloredlogs.install(
+        logger=logger, level=logging.DEBUG if config["debug"] else logging.INFO
+    )
+
+    if config["debug"]:
+        logger.debug(f"arguments: {arguments}")
+
+    try:
+        config["url"] = urllib.parse(arguments.get("--url"))
+    except Exception as e:
+        logger.error(f"Couldn't parse url, found {arguments.get('--url')}")
+        logger.error(e)
+        exit(42)
+
+    # outdir
+    try:
+        if not arguments.get("--outdir"):
+            config["outdir"] = (
+                tempfile.TemporaryDirectory().name + "/bypass-url-parser"
+            )
+        else:
+            config["outdir"] = arguments.get("--outdir")
+        Path(config["outdir"]).mkdir(parents=True, exist_ok=True)
+    except Exception as e:
+        logger.error("Error while creating output directory")
+        logger.error(e)
+        exit(42)
+
+    logger.info("=== Config ===")
+    for key, val in config.items():
+        logger.info(f"{key}: {val}")
+
+    exporter = Bypasser(config)
+    exporter.stage_1()
+    exporter.stage_2()
+    exporter.stage_3()
+    exporter.save_and_quit()
+
+
+if __name__ == "__main__":
+    main()
